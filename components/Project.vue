9a14bee35e8eacc7f5195950fd55e83fa2d4d031
ㅤㅤ <hi@aexhell.life>
2023-07-17 21:06:29 +0200

    feat(components): change color on hover

--- components/Project.vue
+++ components/Project.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="__LAPLACE-PROJECT border-solid border border-gray py-24 px-6 hover:scale-105 relative hover:bg-gray/5 transition">
       <img class="absolute transition-all top-0 left-0 object-cover w-full h-full" :src="`/projects/${data.code}.png`">
-      <div class="__LAPLACE-PROJECT-DETAILS transition-all duration-500 absolute bottom-2 opacity-0 left-12">
+      <div class="__LAPLACE-PROJECT-DETAILS transition-all duration-500 absolute bottom-5 text-white left-4">
          <h3 class="text-sm my-0 font-light" v-text="data.position || 'Front-end'" />
          <h2 class="my-0" v-text="data.title" />
       </div>
@@ -14,7 +14,7 @@
 }
 
 .__LAPLACE-PROJECT:hover .__LAPLACE-PROJECT-DETAILS {
-   @apply opacity-100 left-[-15px]
+   @apply opacity-100 text-black left-[-15px]
 }
 </style>
 
