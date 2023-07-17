e78d4d4a8fd89cd5fd82f18cda0bb0ff2202c399
ㅤㅤ <hi@aexhell.life>
2023-07-17 22:38:56 +0200

    feat(components): proj description

--- components/Project.vue
+++ components/Project.vue
@@ -2,13 +2,22 @@
    <div class="__LAPLACE-PROJECT border-solid border border-gray py-24 px-6 hover:scale-105 relative hover:bg-gray/5 transition">
       <img class="pointer-events-none select-none absolute transition-all top-0 left-0 object-cover w-full h-full" :src="`/projects/${data.code}.png`">
       <div class="__LAPLACE-PROJECT-DETAILS transition-all duration-500 absolute bottom-5 text-white left-4">
-         <h3 class="text-sm my-0 font-light" v-text="data.position || 'Contributor'" />
+         <h3 class="text-xs border-t border-white md:border-solid border-x-0 border-b-0 pt-2 my-0 font-light uppercase" v-text="`[ ${data.position || 'Contributor'} ]`" />
          <h2 class="my-0" v-text="data.title" />
+         <p class="text-sm mb-4" v-text="data.description || 'Description'" />
       </div>
    </div>
 </template>
 
 <style>
+.__LAPLACE-PROJECT-DETAILS h3 {
+   transition: 0.25s border ease-in-out
+}
+
+.__LAPLACE-PROJECT:hover h3.border-white {
+   @apply border-black
+}
+
 .__LAPLACE-PROJECT:hover img {
    opacity: 0.2
 }
