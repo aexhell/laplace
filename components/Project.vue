b4a8129ffabbf74c24dfdfbf898db8dcc74a99ae
ㅤㅤ <hi@aexhell.life>
2023-07-19 01:09:11 +0200

    chore: audio load

--- components/Project.vue
+++ components/Project.vue
@@ -2,8 +2,8 @@
    <div class="__LAPLACE-PROJECT border-solid border border-gray py-24 px-6 hover:scale-105 relative hover:bg-gray/5 transition">
       <img :alt="`${data.title} Image`" class="pointer-events-none select-none absolute transition-all top-0 left-0 object-cover w-full h-full" :src="`/projects/${data.code}.webp`" />
       <div class="__LAPLACE-PROJECT-DETAILS transition-all duration-500 absolute bottom-5 text-white left-6">
-         <h2 class="text-xs border-t border-white md:border-solid border-x-0 border-b-0 pt-2 my-0 font-medium uppercase" v-text="data.position || 'Contributor'" />
-         <h3 class="my-0" v-text="data.title" />
+         <span class="text-xs border-t border-white md:border-solid border-x-0 border-b-0 pt-2 my-0 font-medium uppercase" v-text="data.position || 'Contributor'" />
+         <h2 class="my-0" v-text="data.title" />
          <p class="text-sm mb-4" v-text="data.description || 'Description'" />
       </div>
    </div>
@@ -14,7 +14,7 @@
    transition: 0.25s border ease-in-out
 }
 
-.__LAPLACE-PROJECT:hover h3.border-white {
+.__LAPLACE-PROJECT:hover span.border-white {
    @apply border-black
 }
 
